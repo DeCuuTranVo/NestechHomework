648570f49e77439cdc0c0e942098a989847a87d1
tranvoquang <tranvoquang@itrvn.com>
2023-08-13 19:50:21 +0700

    check empty object

---
+++ homeworks_50problems/others/check_empty_object/index.js
@@ -0,0 +1,26 @@
+/*
+The isEmpty(obj) function should work like this.
+
+let schedule = {};
+alert( isEmpty(schedule) ); // true
+schedule["8:30"] = "get up";
+alert( isEmpty(schedule) ); // false
+*/
+
+function isEmpty(pObj) {
+    let countProperty = 0;
+    for (key in pObj) {
+        countProperty += 1;
+    }
+    
+    if (countProperty > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let schedule = {};
+alert( isEmpty(schedule) ); // true
+schedule["8:30"] = "get up";
+alert( isEmpty(schedule) ); // false
