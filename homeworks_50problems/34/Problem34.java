b478f07de6982786cbefc6da3877dcb0814fd2da
DeCuuTranVo <quangtran.vo.9@gmail.com>
2023-09-04 22:56:29 +0700

    Problem 34 to 43

--- homeworks_50problems/34/Problem34.java
+++ homeworks_50problems/34/Problem34.java
@@ -4,5 +4,30 @@
  */
 
 public class Problem34 {
-    
+    private static int checkNumAndMax(int a, int b) {
+        boolean aIn = ((40 <= a) && (a <= 60));
+        boolean bIn = ((40 <= b) && (b <= 60));
+
+        if (aIn && bIn) {
+            return Math.max(a, b);
+        } else if (aIn && !bIn) {
+            return a;
+        } else if (!aIn && bIn) {
+            return b;
+        } else { // !aIn && !bIn
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        int maxNum = checkNumAndMax(a, b);
+        if (maxNum == -1) {
+            System.out.println("Either a nor b are in range [40,60]");
+        } else {
+            System.out.println("Max and within-ranged number: " + maxNum);
+        }
+    }
 }
